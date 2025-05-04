618139bfd856e425b17f44da7e33f4640a415b81
bleonor <bleonor@sandiego.edu>
2025-05-04 14:25:34 -0700

    Partitioned the data into 3 segemnts, Training, Validation, and Test. Will need to clean
    Attempt at adding regression analysis using interger values that we have: Gross, IMDB Rating, and Year. Appears that the Gross value in correlation to the year and IMDB is VERY low. ~7.6% (using a log transform to reduce outlier impact) Without transform we see it around a 6.8% instead. We may need to look into what was originally taken out and add it back in and test against a smaller subsection.

--- Project_Data_WithNotes.R
+++ Project_Data_WithNotes.R
@@ -153,6 +153,92 @@ ggplot(merged_all_no_duplicates, aes(x = Released_Year, y = Gross)) +
   ) +
   theme_minimal()  # Clean theme
 #Bryan Test Commmit
+#5.4.2025
+
+#Regression
+#Begin by partitioning your original dataset for the regression task into three sets: a 
+#training partition and two holdout partitions.  One holdout partition will be for model 
+#selection (the validation partition).  One partition will be used to report the final model 
+#out-of-sample performance estimate (the testing partition).  I would recommend saving 
+#these three partitions using the write.table() or write.csv() commands (or making sure 
+#you’re all using the same random seed to generate the partitions) so that all group 
+#members start with the same training partition and same holdout partitions so everyone 
+#is making “apples to apples” comparisons. 
 
 
+#Our dataset for now is: "merged_all_no_duplicates" 
+
+#FRACTION OF DATA TO BE USED AS IN-SAMPLE TRAINING DATA
+p1<-.7 #70% FOR TRAINING / Validation, planning for 40% to train, 30% to validate
+p2<- 30/70 #this is the 30% of the the total data
+
+#NUMBER OF OBSERVATIONS IN DATAFRAME
+obs_count<-dim(merged_all_no_duplicates)[1]
+
+#OF OBSERVATIONS IN THE TRAINING DATA (IN-SAMPLE DATA)
+#floor() ROUNDS DOWN TO THE NEAREST WHOLE NUMBER
+training_size <- floor(p1 * obs_count)
+training_size
+#SET THE RANDOM SEED FOR REPRODUCIBILITY
+set.seed(12345)
+#RANDOMLY SHUFFLES THE ROW NUMBERS OF ORIGINAL DATASET
+train_ind <- sample(obs_count, size = training_size)
+Training <- merged_all_no_duplicates[train_ind, ] #PULLS RANDOM ROWS FOR TRAINING 70% which we need to break again
+Testing_Partition <- merged_all_no_duplicates[-train_ind, ] #This is our Testing Partition
+
+
+#We will break the other into validation from the "Training"
+obs_count2<-dim(Training)[1] 
+validation_size <- floor(p2 * obs_count2) #30% of the 70% split before
+set.seed(12345) #set seed
+validation_ind <- sample(obs_count2, size = validation_size) #Same as above for creating the index
+Validation_Partition <- Training[validation_ind,] #PULLS RANDOM ROWS FOR Validation 70% which we need to break again
+Training_Partition <- Training[-validation_ind,] #Remainder go to Training
+
+#Check to see if it adds up
+obs_count == nrow(Validation_Partition) + nrow(Training_Partition) + nrow(Testing_Partition)
+#We now have: Training Partition 40%, Validation Partition 30%, and a Testing Partion 30%
+
+#Creation of a simple Regression model, y = mx + b
+M1 <- lm(Gross~IMDB_Rating + Released_Year,Training_Partition)
+summary(M1)
+PRED_1_IN <- predict(M1, Training_Partition) 
+PRED_1_OUT <- predict(M1, Testing_Partition) 
+
+# Strip names before computing error
+PRED_1_OUT_vec <- unname(PRED_1_OUT)  #fix an issue where we had named numerics, causing NA for Pred_IN
+Gross_OUT_vec <- unname(Testing_Partition$Gross) #fix an issue where we had named numerics, causing NA for Pred_IN
+
+valid_idx <- complete.cases(PRED_1_OUT_vec, Gross_OUT_vec) #fix an issue where RMSE was coming as NA since we had "NA" values
+RMSE_1_OUT <- sqrt(mean((PRED_1_OUT_vec[valid_idx] - Gross_OUT_vec[valid_idx])^2))
+RMSE_1_OUT
+
+# Now do the RMSE
+RMSE_1_OUT <- sqrt(mean((PRED_1_OUT_vec - Gross_OUT_vec)^2))
+RMSE_1_OUT
+summary(M1)
+
+#Plot what we have.
+plot(Training_Partition$IMDB_Rating, Training_Partition$Gross,
+     xlab = "IMDB Rating", ylab = "Gross", main = "Gross vs IMDB Rating")
+abline(M1, col = "red")
+
+#Based off the summary of "M1" we see a R^2 of 0.068 which represents around 6.8% of the gross
+#Data may be showing such a large issue since we have a very very large Gross from Avatar.
+#We can try to visualize this as a percentage of change instead via log transform
+
+# Filter Training and Testing to only rows where Gross > 0
+Training_Partition <- Training_Partition[!is.na(Training_Partition$Gross) & Training_Partition$Gross > 0, ]
+Testing_Partition <- Testing_Partition[!is.na(Testing_Partition$Gross) & Testing_Partition$Gross > 0, ]
+
+# Now create the log variable
+Training_Partition$Log_Gross <- log(Training_Partition$Gross)
+Testing_Partition$Log_Gross <- log(Testing_Partition$Gross)
+
+# Now this will work
+M1_log <- lm(Log_Gross ~ IMDB_Rating + Released_Year, data = Training_Partition)
+summary(M1_log)
+
+#Our data is stil wild, log transformation helped with reducing our R^2, only showing 7.6% now
+#We will need to include more information to see what decides the gross amount.
 
